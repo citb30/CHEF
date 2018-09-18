c1fa5c864653be0d56218b80cca46c35af85809d
carreerit <raghuk.vit@gmail.com>
2018-09-18 06:45:04 +0530

    Update chef-client-init.sh

--- chef-client-init.sh
+++ chef-client-init.sh
@@ -3,4 +3,4 @@
 NODENAME=$(hostname)
 ENVIRONMENT=$(hostname | cut -c -6 | cut -c 4-)
 sed -i -e "s/NODENAME/$NODENAME/" -e "s/ENVIRONMENT/$ENVIRONMENT/" /etc/chef/client.rb
-chef-client
+chef-client -r role[stack]
